bcfadf3317ee94ff19865a29ac7d158429c7d362
Freddy Cervantes <freddycervantes@freddys-mbp.lan>
2019-03-20 16:39:52 -0700

    Created the necessary tables for features

--- db/migrate/20190313055140_create_requests.rb
+++ db/migrate/20190313055140_create_requests.rb
@@ -1,7 +1,11 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-
+      t.integer :request
+      t.references :tutee, foreign_key: true
+      t.references :class, foreign_key: true
+      t.string :subject
+      t.json :meta_values
       t.timestamps
     end
   end
--- db/migrate/20190320225008_create_tutees.rb
+++ db/migrate/20190320225008_create_tutees.rb
@@ -1,6 +1,12 @@
 class CreateTutees < ActiveRecord::Migration[5.2]
   def change
     create_table :tutees do |t|
+      t.integer :tutee
+      t.string :first_name
+      t.string :last_name
+      t.boolean :privilege
+      t.json :meta_values
+      t.timestamps
     end
   end
 end

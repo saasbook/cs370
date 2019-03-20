c7da3ae6e7b02c73b8b0da161d602146a92e33c9
Freddy Cervantes <freddycervantes@freddys-mbp.lan>
2019-03-20 16:39:52 -0700

    Created the necessary tables for features

--- db/migrate/20190320225124_create_class.rb
+++ db/migrate/20190320225124_create_class.rb
@@ -1,6 +1,11 @@
 class CreateClass < ActiveRecord::Migration[5.2]
   def change
     create_table :classes do |t|
+      t.integer :class
+      t.string :name
+      t.string :semester
+      t.json :meta_values
+      t.timestamps
     end
   end
 end

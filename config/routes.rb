7210486318d13c75e184ded1774f7ab9286a3cba
Freddy Cervantes <freddycervantes@Freddys-MacBook-Pro.local>
2019-03-20 21:26:31 -0700

    added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tutees
+  resources :courses
+  resources :requests
+
 end

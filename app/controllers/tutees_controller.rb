045e37181b4fbf0fc2066880288202df719eecd3
“freddycervantes” <freddycervantes@berkeley.edu>
2019-03-20 21:53:17 -0700

    Added blank methods and views for each table

--- app/controllers/tutees_controller.rb
+++ app/controllers/tutees_controller.rb
@@ -1,2 +1,23 @@
 class TuteesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tutees
-  resources :courses
-  resources :requests
+  resources :tutees, :courses, :requests
 
 end

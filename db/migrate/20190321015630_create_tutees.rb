cbca2787d71ce9442524a854cb04ca7dda3e140d
“freddycervantes” <freddycervantes@berkeley.edu>
2019-03-20 19:45:46 -0700

    updated tables

--- db/migrate/20190321015630_create_tutees.rb
+++ db/migrate/20190321015630_create_tutees.rb
@@ -1,7 +1,7 @@
 class CreateTutees < ActiveRecord::Migration[5.2]
   def change
     create_table :tutees do |t|
-      t.integer :tutee
+      t.integer :sid
       t.string :first_name
       t.string :last_name
       t.string :email

a704260b81c4b08efbf6941c205d318ec393e9fe
Hector Aguilar <ah91086@berkeley.edu>
2019-03-20 22:13:18 -0700

    middle of adding editing profile functionality

--- app/models/berkeley_class.rb
+++ app/models/berkeley_class.rb
@@ -2,10 +2,20 @@ class BerkeleyClass < ApplicationRecord
 	def true_classes
 		@preferred_classes = []
 		self.attributes.each_pair do |name, value|
-	      	if value == true
+	      	if value
 	        	@preferred_classes.push(name)
 	      	end
       	end
       	@preferred_classes
 	end
+
+	def all_classes
+		@preferred_classes = []
+		self.attributes.each_pair do |name, value|
+	        @preferred_classes.push(name)
+      	end
+      	@preferred_classes
+
+	end
+
 end

5e0da8ff54c6a5388f1319bfea398c08f2114079
Duy Nguyen <duy@berkeley.edu>
2019-03-21 21:50:17 -0700

    MVC without gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tutees, :courses, :requests
 
+  get 'requests/history/:tutee_id', to: 'requests#history', as: :request_history_tutee  
 end

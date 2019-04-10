721f172b6bb7cabb47d2207e25ccc4c8b2664a32
Alvin Nguyen <alvinnguyen116@gmail.com>
2019-04-09 18:08:05 -0700

    replace factory bot with factory girl

--- spec/factories/core.rb
+++ spec/factories/core.rb
@@ -1,13 +1,22 @@
-FactoryBot.define do
+FactoryGirl.define do
 	begin 
-		factory :tutor
+		factory :tutor do 
+			type_of_tutor "CSM"
+			grade_level 'SENIOR'
+			first_name 'ALVIN'
+			last_name 'NGUYEN'
+		end
+	rescue FactoryGirl::DuplicateDefinitionError => msg 
+		puts msg
+ 	end	
+ 	begin 
 		factory :request
+	rescue FactoryGirl::DuplicateDefinitionError => msg 
+		puts msg
+ 	end
 
 		factory :meeting do
 			tutor
 			request
-		end
-	rescue FactoryBot::DuplicateDefinitionError => msg 
-		puts msg
- 	end	
+		end	
 end

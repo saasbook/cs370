d1f5f471ea6816552c8e3e4ebc356f279bf467d2
Duy Nguyen <duy@berkeley.edu>
2019-04-10 22:37:38 -0700

    add spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tutees, :courses, :requests
 
-  get 'requests/history/:tutee_id', to: 'requests#history', as: :request_history_tutee  
+  # get 'requests/history/:tutee_id', to: 'requests#history', as: :request_history_tutee
+  get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee
+
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   before(:each) do
-    @request = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
+    # @request = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
+    @request = Request.new(:tutee_id => 1, :course_id => 1, :subject => "tree")
   end
   describe "view my history request" do
     it "start with correct tutee and tutee's info" do

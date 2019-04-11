70d2b905a19e6a639bff1ca250017075c154f84d
Duy Nguyen <duy@berkeley.edu>
2019-04-10 22:37:38 -0700

    add spec

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe RequestsController, type: :controller do
   describe "show history" do
@@ -6,19 +7,23 @@ RSpec.describe RequestsController, type: :controller do
       @m = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
     end
     it "it should generate routing for history" do
-      {:get => 'requests/history/1'}.
+      {:get => request_history_tutee_path(1)}.
       should route_to(:controller => "requests", :action => "history", :tutee_id => "1")
     end
-    # it "it should call a correct method history" do
-    #   # expect(Request).to receive(:history).with('1')
-    #   # get :show, {:id => "1"}
-    #   Request.should receive(:find).with(1)
-    #   get :history, {:tutee_id => 1}
-    # end
-    it "makes the history results available to that template" do
-      Request.stub(:find).with('1')
-      get :history, {:tutee_id => "1"}
-      response.should render_template('history')
+    it "it should call a correct method history" do
+      # expect(Request).to receive(:history).with('1')
+      # get :show, {:id => "1"}
+      Request.should_receive(:where).with(:tutee_id => "1")
+
+      # Request.should_receive(:find).with('1')
+      get :history,  params: {:tutee_id => "1"}
     end
+
+    it "makes the history results available to that template" do
+      Request.should_receive(:where).with(:tutee_id => "1")
+      get :history,  params: {:tutee_id => "1"}
+      response.should render_template("history")
+    end
+
   end
 end

4f3f252aa0b485a75c8799fdbf58c83f979617dd
Phu Ho <fooho264@gmail.com>
2019-04-10 20:47:11 -0700

    rspec

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Request, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @request = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
+  end
+  describe "view my history request" do
+    it "start with correct tutee and tutee's info" do
+      expect(@request.tutee_id).to eq(1)
+      expect(@request.course_id).to eq(1)
+      expect(@request.subject).to eq "tree"
+    end
+  end
 end

e6ca85bf8f9022d3eb021f2a977ffa0dac931312
Freddy Cervantes <freddycervantes@Freddys-MacBook-Pro.local>
2019-04-12 15:01:07 -0700

    simple log in

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tutees, :courses, :requests
   root :to => redirect('/tutees')
-
+  post 'tutees/login/' => 'tutees#login', as: :login_tutee
 end

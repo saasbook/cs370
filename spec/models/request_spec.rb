220b1f681d1a229de43c916e310fd959d128c5fc
Phu Ho <fooho264@gmail.com>
2019-04-12 12:22:44 -0700

    rspec model

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe Request, type: :model do
     # @request = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
     @request = Request.new(:tutee_id => 1, :course_id => 1, :subject => "tree")
   end
-  describe "view my history request" do
-    it "start with correct tutee and tutee's info" do
-      expect(@request.tutee_id).to eq(1)
-      expect(@request.course_id).to eq(1)
-      expect(@request.subject).to eq "tree"
-    end
+  # describe "view my history request" do
+  #   it "start with correct tutee and tutee's info" do
+  #     expect(@request.tutee_id).to eq(1)
+  #     expect(@request.course_id).to eq(1)
+  #     expect(@request.subject).to eq "tree"
+  #   end
+  # end
+  it "has a tutee_id" do
+    expect(@request.tutee_id).not_to be_nil
+  end
+  it "could find a correct tutee id" do
+    expect(@request.tutee_id).to eq(1)
+  end
+  it "could find a correct course_id" do
+    expect(@request.course_id).to eq(1)
+  end
+  it "could find a correct subject"do
+    expect(@request.subject).to eq("tree")
   end
 end

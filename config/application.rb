d9a0161184a6dae1531e83d012251200866a04a8
“freddycervantes” <freddycervantes@berkeley.edu>
2019-04-11 21:35:02 -0700

    Setting up Rspec

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,14 @@ module Cs370
     config.generators.system_tests = nil
   end
 end
+
+config.generators do |g|
+  g.test_framework :rspec,
+                   :fixtures => true,
+                   :view_specs => false,
+                   :helper_specs => false,
+                   :routing_specs => false,
+                   :controller_specs => true,
+                   :request_specs => true
+  g.fixture_replacement :factory_girl, :dir => "spec/factories"
+end
--- spec/controllers/tutees_controller_spec.rb
+++ spec/controllers/tutees_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe TuteesController, type: :controller do
+RSpec.describe TuteesController do
+  describe "GET #index" do
+    it "redirects to new"
+  end
 
+  describe "GET #show" do
+    it "renders the :show template"
+    it "displays correct username"
+  end
+
+  describe "GET #new" do
+    it "assigns a new tutee to @tutee"
+    it "renders the :new template"
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new contact in the database"
+      it "redirects to the home page"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new contact in the database"
+      it "re-renders the :new template"
+    end
+  end
 end

98326e0cd612d49f2ffd224d69c23a69edd9fadc
Freddy Cervantes <freddycervantes@airbears2-10-142-37-29.airbears2.1918.berkeley.edu>
2019-04-14 13:23:33 -0700

    Template for rspec

--- spec/controllers/tutees_controller_spec.rb
+++ spec/controllers/tutees_controller_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe TuteesController, type :controller do
 
   describe "GET #index" do
     before :each do
-      @tmp_tutee = Tutee.create()
+      # @tmp_tutee = Tutee.create()
     end
-    it "redirects to new"
+    it "without existing account renders :new template"
+    it "with existing account renders :show template"
   end
 
   describe "GET #show" do
@@ -14,12 +15,40 @@ RSpec.describe TuteesController, type :controller do
     it "displays correct username"
   end
 
+  describe "POST #login" do
+    it "renders the :show template"
+    it "displays correct username"
+  end
+
+  describe "GET #edit" do
+    it "renders the :edit template"
+    it "retrieves correct tutee information"
+  end
+
   describe "GET #new" do
-    it "assigns a new tutee to @tutee"
     it "renders the :new template"
   end
 
   describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new contact in the database"
+      it "given valid inputs renders :show template"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new contact in the database"
+      it "re-renders the :new template"
+      it "is invalid without first name"
+      it "is invalid without first name"
+      it "is invalid without sid"
+      it "is invalid without email"
+      it "is invalid without @berkeley.edu email"
+      it "is invalid without birthdate"
+      it "is invalid without proper birthdate format"
+      it "is invalid if the birthdate is in the future"
+    end
+  end
+  describe "PUT #update" do
     context "with valid attributes" do
       it "saves the new contact in the database"
       it "redirects to the home page"
@@ -27,7 +56,15 @@ RSpec.describe TuteesController, type :controller do
 
     context "with invalid attributes" do
       it "does not save the new contact in the database"
-      it "re-renders the :new template"
+      it "re-renders the :edit template"
+      it "is invalid without first name"
+      it "is invalid without first name"
+      it "is invalid without sid"
+      it "is invalid without email"
+      it "is invalid without @berkeley.edu email"
+      it "is invalid without birthdate"
+      it "is invalid without proper birthdate format"
+      it "is invalid if the birthdate is in the future"
     end
   end
 end

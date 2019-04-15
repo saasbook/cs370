12e159ea0edf01231b72d0fbfb990ab9ee5e7959
tnguyen <thu.nguyen@vudu.com>
2019-04-14 20:45:32 -0700

    Added views for requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tutees, :courses, :requests
-
+  root :to => redirect('/tutees')
+  post 'tutees/login/' => 'tutees#login', as: :login_tutee
   get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee
-  get 'tutees/:id' => 'tutees#show', as: :tutee_homepage
+
 end

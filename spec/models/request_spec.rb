a0d08c53033d295c52e662e37937edc01a29afc6
Duy Nguyen <duy@berkeley.edu>
2019-04-14 20:21:54 -0700

    add rspec

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Request, type: :model do
   before(:each) do
     # @request = double("Request", :tutee_id => 1, :course_id => 1, :subject => "tree")
     # @request = Request.new(:tutee_id => 1, :course_id => 1, :subject => "tree")
-    # @course = create(:course, :id => 1)
-    # @tutee = create(:tutee)
+    @course = create(:course)
+    @tutee = create(:tutee)
     # @request = create(:request, :tutee_id => @tutee.id, :course_id => @course.id, :subject => "tree")
-    @request = create(:request)
+    @request = create(:request, :tutee => @tutee, :course => @course, :subject => "tree")
   end
   # describe "view my history request" do
   #   it "start with correct tutee and tutee's info" do
@@ -21,12 +21,28 @@ RSpec.describe Request, type: :model do
     expect(@request.tutee_id).not_to be_nil
   end
   it "could find a correct tutee id" do
-    expect(@request.tutee_id).to eq(10)
+    expect(@request.tutee_id).to eq(@tutee.id)
   end
   it "could find a correct course_id" do
-    expect(@request.course_id).to eq(1)
+    expect(@request.course_id).to eq(@course.id)
   end
   it "could find a correct subject"do
     expect(@request.subject).to eq("tree")
   end
+  it 'requires a tutee parameter' do
+    expect { create(:request, tutee: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'has a course' do
+    expect(@request.course).not_to be_nil
+  end
+
+  it 'requires a course parameter' do
+    expect { create(:request, course: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  #
+  # it 'requires a request parameter' do
+  #   expect { create(:meeting, request: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  # end
+
 end

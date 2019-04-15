98e68d90b859665aea917a5d1aa1d6c7468f4bf9
tnguyen <thu.nguyen@vudu.com>
2019-04-14 23:13:13 -0700

    Completed views for create new tutoring requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tutees, :courses, :requests
+  resources :tutees, :courses
+  resources :tutees do
+    resources :requests
+  end
   root :to => redirect('/tutees')
   post 'tutees/login/' => 'tutees#login', as: :login_tutee
   get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee

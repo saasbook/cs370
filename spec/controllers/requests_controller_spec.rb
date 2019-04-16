a9df397802817dfb463a7d073f365d3d09ec2f0b
tnguyen <thu.nguyen@vudu.com>
2019-04-16 11:21:44 -0700

    Model rspec coverage 100%

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -22,14 +22,14 @@ RSpec.describe RequestsController, type: :controller do
     # end
   end
   
-  # describe "create tutoring request" do
-  #   before :each do
-  #     @oski = double("Tutee", :sid=>2, :first_name=>"an", :email=>"an.ju@cal.ber")
-  #   end
-  #   it "it should generate routing for tutee" do
-  #     {:get => tutee_homepage_path(1)}.
-  #         should route_to(:controller => "tutees", :action => "show", :id => "1")
-  #   end
-  #
-  # end
+  describe "create tutoring request" do
+    before :each do
+      @oski = double("Tutee", :sid=>1, :first_name=>"an", :email=>"an.ju@cal.ber")
+    end
+    it "it should generate routing for tutee" do
+      {:get => tutee_path(1)}.
+          should route_to(:controller => "tutees", :action => "show", :id => "1")
+    end
+
+  end
 end

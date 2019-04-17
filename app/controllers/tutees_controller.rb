807f79c9047572683d7379ba139484c3614f82ad
Freddy Cervantes <freddycervantes@freddys-mbp.lan>
2019-04-17 03:11:18 -0700

    Eliminated the smell

--- app/controllers/tutees_controller.rb
+++ app/controllers/tutees_controller.rb
@@ -35,51 +35,52 @@ class TuteesController < ApplicationController
   def create
     tutee_params[:email] = tutee_params[:email].downcase!
     # Checks if parameters are good
-    if tutee_params[:first_name] == "" or tutee_params[:last_name] == ""
-      flash[:message] = "First Name or Last Name left blank."
-      redirect_to new_tutee_path
-    elsif tutee_params[:first_name] =~ /\d/ or tutee_params[:last_name] =~ /\d/
-      flash[:message] = "No digits in first or last name."
-      redirect_to new_tutee_path
-    elsif tutee_params[:sid].blank?
-      flash[:message] = "SID field cannot be left empty"
-      redirect_to new_tutee_path
-    elsif not tutee_params[:email].ends_with? "@berkeley.edu" or tutee_params[:email].blank?
-      flash[:message] = "Invalid email or missing email, Note: email must with @berkeley.edu."
-      redirect_to new_tutee_path
-    elsif not tutee_params[:birthdate].match(/\d{4}-\d{2}-\d{2}/) or tutee_params[:birthdate] == "" or tutee_params[:birthdate] > Time.now.strftime("%Y-%m-%d")
-      flash[:message] = "Invalid date or date format, or empty date field."
-      redirect_to new_tutee_path
-    else
-      @tutee = Tutee.create!(tutee_params)
-      flash[:message] = "Account for #{@tutee.first_name} was successfully created."
-    end
+    # if tutee_params[:first_name] == "" or tutee_params[:last_name] == ""
+    #   flash[:message] = "First Name or Last Name left blank."
+    #   redirect_to new_tutee_path
+    # elsif tutee_params[:first_name] =~ /\d/ or tutee_params[:last_name] =~ /\d/
+    #   flash[:message] = "No digits in first or last name."
+    #   redirect_to new_tutee_path
+    # elsif tutee_params[:sid].blank?
+    #   flash[:message] = "SID field cannot be left empty"
+    #   redirect_to new_tutee_path
+    # elsif not tutee_params[:email].ends_with? "@berkeley.edu" or tutee_params[:email].blank?
+    #   flash[:message] = "Invalid email or missing email, Note: email must with @berkeley.edu."
+    #   redirect_to new_tutee_path
+    # elsif not tutee_params[:birthdate].match(/\d{4}-\d{2}-\d{2}/) or tutee_params[:birthdate] == "" or tutee_params[:birthdate] > Time.now.strftime("%Y-%m-%d")
+    #   flash[:message] = "Invalid date or date format, or empty date field."
+    #   redirect_to new_tutee_path
+    # else
+    @tutee = Tutee.create!(tutee_params)
+    flash[:message] = "Account for #{@tutee.first_name} was successfully created."
+    redirect_to tutee_path(@tutee)
+    # end
   end
 
   def update
     @tutee = Tutee.find params[:id]
     tutee_params[:email] = tutee_params[:email].downcase!
     # Checks if parameters are good
-    if tutee_params[:first_name] == "" or tutee_params[:last_name] == ""
-      flash[:message] = "First or Last name left cannot be left blank."
-      redirect_to edit_tutee_path(@tutee)
-    elsif tutee_params[:first_name] =~ /\d/ or tutee_params[:last_name] =~ /\d/
-      flash[:message] = "No digits in first or last name."
-      redirect_to edit_tutee_path(@tutee)
-    elsif tutee_params[:sid].blank?
-      flash[:message] = "SID field cannot be left empty"
-      redirect_to edit_tutee_path(@tutee)
-    elsif not tutee_params[:email].downcase.ends_with? "@berkeley.edu" or tutee_params[:email].blank?
-      flash[:message] = "Invalid email or missing email, Note: email must with @berkeley.edu."
-      redirect_to edit_tutee_path(@tutee)
-    elsif not tutee_params[:birthdate].match(/\d{4}-\d{2}-\d{2}/) or tutee_params[:birthdate] == "" or tutee_params[:birthdate] > Time.now.strftime("%Y-%m-%d")
-      flash[:message] = "Invalid date or date format, or empty date field."
-      redirect_to edit_tutee_path(@tutee)
-    else
-        @tutee.update!(tutee_params)
-        flash[:message] = "Information was successfully updated."
-        redirect_to tutee_path(@tutee)
-    end
+    # if tutee_params[:first_name] == "" or tutee_params[:last_name] == ""
+    #   flash[:message] = "First or Last name left cannot be left blank."
+    #   redirect_to edit_tutee_path(@tutee)
+    # elsif tutee_params[:first_name] =~ /\d/ or tutee_params[:last_name] =~ /\d/
+    #   flash[:message] = "No digits in first or last name."
+    #   redirect_to edit_tutee_path(@tutee)
+    # elsif tutee_params[:sid].blank?
+    #   flash[:message] = "SID field cannot be left empty"
+    #   redirect_to edit_tutee_path(@tutee)
+    # elsif not tutee_params[:email].downcase.ends_with? "@berkeley.edu" or tutee_params[:email].blank?
+    #   flash[:message] = "Invalid email or missing email, Note: email must with @berkeley.edu."
+    #   redirect_to edit_tutee_path(@tutee)
+    # elsif not tutee_params[:birthdate].match(/\d{4}-\d{2}-\d{2}/) or tutee_params[:birthdate] == "" or tutee_params[:birthdate] > Time.now.strftime("%Y-%m-%d")
+    #   flash[:message] = "Invalid date or date format, or empty date field."
+    #   redirect_to edit_tutee_path(@tutee)
+    # else
+    @tutee.update!(tutee_params)
+    flash[:message] = "Information was successfully updated."
+    redirect_to tutee_path(@tutee)
+    # end
   end
 
   def destroy

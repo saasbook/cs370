fab8f5cba87a6021e172ff0b22cf04a31b71dfb5
tnguyen <thu.nguyen@vudu.com>
2019-04-18 22:14:24 -0700

    Pull request_history from history-requests-feature. Model coverage 100%. Controller coverage 96%

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    Admin.new(password_digest: 'thisIsPassWord', current_semester: 'Sp2019', statistics_semester: 'Sp2019')
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "has a current semester" do
+    expect(subject.current_semester).not_to be_nil
+  end
+
+  it "has a statistic semester" do
+    expect(subject.statistics_semester).not_to be_nil
+  end
+
+  it "has a master admin index number" do
+    expect(Admin.master_admin_index).not_to be_nil
+  end
+
+
 end

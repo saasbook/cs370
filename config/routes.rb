f01797e3914b6fd22460cba2b6831d3c0e088818
Freddy Cervantes <freddycervantes@freddys-mbp.lan>
2019-04-18 23:20:07 -0700

    Created evaluation setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'evaluations/new'
+  get 'evaluations/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tutees, :courses
   resources :tutees do
--- db/migrate/20190313055153_create_evaluations.rb
+++ db/migrate/20190313055153_create_evaluations.rb
@@ -1,7 +1,17 @@
 class CreateEvaluations < ActiveRecord::Migration[5.2]
   def change
     create_table :evaluations do |t|
-
+      t.references :meeting, foreign_key: true
+      t.string :topics
+      t.integer :hours
+      t.text :positive
+      t.text :best
+      t.text :feedback
+      t.integer :knowledgeable
+      t.integer :helpful
+      t.integer :clarity
+      t.integer :pacing
+      t.text :final_comments
       t.timestamps
     end
   end

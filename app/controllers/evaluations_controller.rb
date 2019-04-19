99e1538d93b18d4e78f361d4ee73a2daff78046b
Freddy Cervantes <freddycervantes@freddys-mbp.lan>
2019-04-19 08:57:49 -0700

    Added eval_param requirements to controller

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,4 +1,7 @@
 class EvaluationsController < ApplicationController
+  def evaluation_params
+    params.require(:evaluation).permit(:topics, :hours, :positive, :best, :feedback, :knowledgeable, :helpful, :clarity, :pacing, :final_comments, :meeting)
+  end
   def new
   end
 

04415b9bd9c1bafcbd493a1a24bc6b5c338a1471
Freddy Cervantes <freddycervantes@Freddys-MacBook-Pro.local>
2019-04-22 23:00:18 -0700

    Added only wanted files

--- app/models/tutee.rb
+++ app/models/tutee.rb
@@ -1,4 +1,8 @@
 class Tutee < ApplicationRecord
   has_many :requests
-  has_many :tutors, through: :requests
+  has_many :courses, through: :requests
+  scope :pending_evaluations, lambda { |student|
+      Evaluation.find_by_id(Tutee.joins(:requests, :meetings, :evaluation).where(tutee.id: student).where(evaluation.status: 'pending').uniq)}
+
+  end
 end

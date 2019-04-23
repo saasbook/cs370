19301e87a2347123de3cf389002322fae1aec259
Phu Ho <fooho264@gmail.com>
2019-04-22 22:45:02 -0700

    starting

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'evaluations/new'
-  get 'evaluations/create'
+  # get 'evaluations/new'
+  # get 'evaluations/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tutees, :courses
+  resources :tutees, :courses, :evaluations
   resources :tutees do
-    resources :requests
+    resources :requests, :evaluations
   end
   post 'tutees/login/' => 'tutees#login', as: :login_tutee
   get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee
 
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
   resources :tutors
 
 end
-
--- db/migrate/20190313055153_create_evaluations.rb
+++ db/migrate/20190313055153_create_evaluations.rb
@@ -2,15 +2,16 @@ class CreateEvaluations < ActiveRecord::Migration[5.2]
   def change
     create_table :evaluations do |t|
       t.string :topics
-      t.integer :hours
+      t.float :hours, :precision => 3, :scale => 2
       t.text :positive
       t.text :best
       t.text :feedback
-      t.integer :knowledgeable
-      t.integer :helpful
-      t.integer :clarity
-      t.integer :pacing
+      t.integer :knowledgeable, :limit => 1
+      t.integer :helpful, :limit => 1
+      t.integer :clarity, :limit => 1
+      t.integer :pacing, :limit => 1
       t.text :final_comments
+      t.string :status, :default => "Pending"
       t.timestamps
     end
   end

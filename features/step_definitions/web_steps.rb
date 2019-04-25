a00baf8ccd59a11ba569ca7f149c1a087955d7f5
tnguyen <thu.nguyen@vudu.com>
2019-04-24 19:11:43 -0700

    Finished tutee can request with hours feature

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -37,9 +37,9 @@ World(WithinHelpers)
 # end
 
 # Multi-line step scoper
-When /^(.*) within (.*[^:]):$/ do |step, parent, table_or_string|
-  with_scope(parent) { When "#{step}:", table_or_string }
-end
+# When /^(.*) within (.*[^:]):$/ do |step, parent, table_or_string|
+#   with_scope(parent) { When "#{step}:", table_or_string }
+# end
 #
 # Given /^(?:|I )am on (.+)$/ do |page_name|
 #   visit path_to(page_name)

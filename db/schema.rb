ed29dd9f2543127693b08ff86e74879f06ea0847
Duy Nguyen <duy@berkeley.edu>
2019-04-27 11:58:29 -0700

    add private method to createSession

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_023805) do
+ActiveRecord::Schema.define(version: 2019_04_27_180722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2019_04_19_023805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.index ["email"], name: "index_tutees_on_email", unique: true
   end
 
   create_table "tutors", force: :cascade do |t|

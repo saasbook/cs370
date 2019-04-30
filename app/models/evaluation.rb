504aba2a38b25791f82a4df9a9899eff9d1b174d
Freddy Cervantes <freddycervantes@Freddys-MacBook-Pro.local>
2019-04-30 14:22:00 -0700

    Added took place to history table

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -10,7 +10,7 @@ class Evaluation < ApplicationRecord
   validates :hours, presence: true, numericality: {only_float: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}, on: :update, :if => :took_place
   validates :positive, presence: true, on: :update, :if => :took_place
   validates :best, presence: true, on: :update, :if => :took_place
-  validates :feedback, presence: true, on: :update, :if => :took_place.equal?('true')
+  validates :feedback, presence: true, on: :update, :if => :took_place
   validates :knowledgeable, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Must be valid rating integer"}, on: :update, :if => :took_place
   validates :helpful, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Must be valid rating integer"}, on: :update, :if => :took_place
   validates :clarity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Must be valid rating integer"}, on: :update, :if => :took_place

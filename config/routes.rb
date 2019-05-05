ed1f639e63ceab27dc7e3fd081ef66f48a8cec2f
Alvin Nguyen <alvinnguyen116@gmail.com>
2019-05-05 14:11:36 -0700

    Priorityandmatching (#13)
    * Did find students
    
    * Added bootstrap to make the find students page look better
    
    * Fixed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   get  '/welcome/tutor' => 'welcome#tutor', as: :welcome_tutor
   post 'tutees/login/' => 'tutees#login', as: :login_tutee
   get 'requests/history/:tutee_id' => 'requests#history', as: :request_history_tutee
-  
   get '/tutors/:tutor_id/find_students' => 'tutors#find_students', as: :tutor_find_students
   get '/tutors/:tutor_id/requests/email/' => 'requests#email', as: :requests_email_tutor
-  
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
 

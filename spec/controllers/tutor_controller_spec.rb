edf5d1391457579f8d2de55f08e852f4ef1bbeb9
divineechidume@berkeley.edu <divineechidume@berkeley.edu>
2019-05-05 13:09:28 -0700

    got it to show total number f hours per week

--- spec/controllers/tutor_controller_spec.rb
+++ spec/controllers/tutor_controller_spec.rb
@@ -6,23 +6,22 @@ describe TutorsController do
   fixtures :evaluations, :meetings
   describe 'tutor metrics' do
     let!(:tutor) {Tutor.create()}
-    let!(:eval1) { Evaluation.create()}
     zone = ActiveSupport::TimeZone.new('Hawaii')
-    it 'expects to have 22 total hours' do
+    it 'expects to have 27 total hours' do
       @tutor1 = FactoryBot.create(:tutor, :id => 1)
-      expect(Tutor.total_hours_helper(@tutor1)).to eq(22)
+      expect(Tutor.total_hours_helper(@tutor1)).to eq(27)
     end
 
     it 'expects to have 22 hours done this week' do
-      allow(Time).to receive(:now).and_return(Time.mktime(2019,04,28,11,00,00).gmtime)
+      allow(Time).to receive(:now).and_return(Time.mktime(2019,04,25,11,00,00).gmtime)
       @tutor1 = FactoryBot.create(:tutor, :id => 1)
       expect(Tutor.hours_this_week_helper(@tutor1)).to eq(22)
     end
 
-    it 'expects to have 22 on average' do
-      allow(Time).to receive(:now).and_return(Time.mktime(2019,04,28,11,00,00).gmtime)
+    it 'expects to have 6.09 on average' do
+      allow(Time).to receive(:now).and_return(Time.mktime(2019,04,25,11,00,00).gmtime)
       @tutor1 = FactoryBot.create(:tutor, :id => 1)
-      expect(Tutor.average_hours_helper(@tutor1)).to eq(22)
+      expect(Tutor.average_hours_helper(@tutor1)).to eq(6.096774193548387)
     end
   end
 end

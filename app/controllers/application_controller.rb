1e4a83b12c01e8f001cb6abdc28b5ec2eb3d1cfd
Duy Nguyen <duy@berkeley.edu>
2019-05-05 23:14:19 -0700

    change submit form link in new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :sid, :privilege, :email, :birthdate, :gender, :ethnicity,
+                                  :major, :dsp, :transfer, :year, :pronoun])
+  end
+
+  def after_sign_in_path_for(resource)
+  # return the path based on resource
+    p resource
+    p tutee_path(resource)
+    tutee_path(resource)
+  end
   private
     def check_tutee_logged_in
       tutee_id = params.has_key?(:tutee_id) ? params[:tutee_id] : -1
       if tutee_id == -1 and params.has_key?(:id)
         tutee_id = params[:id]
-      end 
+      end
 
       if !(session[:tutee_id].to_i == tutee_id.to_i)
         redirect_to tutees_path

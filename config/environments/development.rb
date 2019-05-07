79d5465b232fa4afb004855ebe7a9eba9445c7ba
Tina Taleb <tinataleb@berkeley.edu>
2019-05-06 18:19:52 -0700

    cannot use personal email to test if the confirmation email is sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
+  # *************** MAILER ******************
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }

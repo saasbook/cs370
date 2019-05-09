cb44b00611808213571d0a4bef6e651cb180cf5c
divineechidume@berkeley.edu <divineechidume@berkeley.edu>
2019-05-09 03:29:37 -0700

    modified addtional tests made for tutormetrics

--- spec/controllers/tutor_controller_spec.rb
+++ spec/controllers/tutor_controller_spec.rb
@@ -8,19 +8,19 @@ describe TutorsController do
     let!(:tutor) {Tutor.create()}
     zone = ActiveSupport::TimeZone.new('Hawaii')
     it 'expects to have 27 total hours' do
-      @tutor1 = FactoryBot.create(:tutor, :id => 1)
+      @tutor1 = FactoryBot.create(:tutor, :id => 20)
       expect(Tutor.total_hours_helper(@tutor1)).to eq(27)
     end
 
     it 'expects to have 22 hours done this week' do
       allow(Time).to receive(:now).and_return(Time.mktime(2019,04,25,11,00,00).gmtime)
-      @tutor1 = FactoryBot.create(:tutor, :id => 1)
+      @tutor1 = FactoryBot.create(:tutor, :id => 20)
       expect(Tutor.hours_this_week_helper(@tutor1)).to eq(22)
     end
 
     it 'expects to have 6.09 on average' do
       allow(Time).to receive(:now).and_return(Time.mktime(2019,04,25,11,00,00).gmtime)
-      @tutor1 = FactoryBot.create(:tutor, :id => 1)
+      @tutor1 = FactoryBot.create(:tutor, :id => 20)
       expect(Tutor.average_hours_helper(@tutor1)).to eq(6.096774193548387)
     end
   end

26b4c0e911d31de6a301154c64f7a2f9f41298ae
freddycervantes <freddycervantes@berkeley.edu>
2019-05-08 21:34:25 -0700

    Cleaned up Tutee

--- app/controllers/tutees_controller.rb
+++ app/controllers/tutees_controller.rb
@@ -1,40 +1,10 @@
 class TuteesController < ApplicationController
   layout 'tutee_layout', :only => [:show, :edit]
   # Authorization section
-  #before_action :set_tutee, expect: [:index,:login, :createTuteeSession, :new, :create]
-  # skip_before_action :verify_authenticity_token, only: [:createTuteeSession]
   before_action :check_tutee_logged_in, except: [:index, :login, :createTuteeSession, :new, :create]
 
-  # def createTuteeSession
-  #   #Add authentication here in the future
-  #   @tutee = Tutee.find_by_id(params[:id])
-  #   if @tutee.nil?
-  #     redirect_to new_tutee_registration_path
-  #   elsif @tutee
-  #     add_tutee_to_session(@tutee)
-  #   else
-  #     new_tutee_session_path
-  #   end
-  # end
-
-  # def destroyTuteeSession
-  #   session[:tutee_logged_in] = false
-  #   session[:tutee_id] = nil
-  #   redirect_to destroy_tutee_session_path, method: :delete
 
 
-  # end
-
-
-  def tutee_params
-  #   params.require(:tutee).permit(:first_name, :last_name, :sid, :privilege, :email, :birthdate, :gender, :ethnicity,
-  #                                 :major, :dsp, :transfer, :year, :pronoun)
-  end
-
-  def login
-    # @tutee = Tutee.find_by_id(params[:id])
-    # redirect_to tutee_path(@tutee)
-  end
 
   def index
   end
@@ -58,14 +28,4 @@ class TuteesController < ApplicationController
   def update
   end
 
-  # private
-  #   def set_tutee
-  #     @tutee = Tutee.find_by_id(session[:tutee_id])
-  #   end
-
-
-    # def add_tutee_to_session tutee
-    #   session[:tutee_id] = @tutee.id
-    #   redirect_to tutee_path(@tutee)
-    # end
 end

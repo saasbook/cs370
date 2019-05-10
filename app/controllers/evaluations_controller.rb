c3438dd88ca7d7df45d8961f80fab553857bdc82
Phu Ho <fooho264@gmail.com>
2019-05-10 13:17:39 -0700

    Fix tutee_id error rspec

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -2,9 +2,9 @@ class EvaluationsController < ApplicationController
   def evaluation_params
     params.require(:evaluation).permit(:topics, :hours, :positive, :best, :feedback, :knowledgeable, :helpful, :clarity, :pacing, :final_comments, :took_place, :status, :hash_id)
   end
-  def new
-    # @tutee = Tutee.find params[:tutee_id]
-  end
+  # def new
+  #   @tutee = Tutee.find params[:tutee_id]
+  # end
 
   def create
     # @tutee = Tutee.find_by_id(params[:id])
--- spec/controllers/evaluations_controller_spec.rb
+++ spec/controllers/evaluations_controller_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 
 RSpec.describe EvaluationsController, type: :controller do
   before :each do
-    @tutee = Tutee.create(:id => 1, :sid => 123456789, :first_name => "Bob", :last_name => "Burgers", :email => "bobs@berkeley.edu", :birthdate => "1992-01-01", :privilege => 'No', :gender => 'male',
-              :ethnicity => 'Asian', :dsp => 'Yes', :transfer => 'Yes', :year => '4 years', :pronoun => 'he/his', :major => 'EECS')
+    @tutee = Tutee.create(:sid=>1234567, :first_name=>"an", :email=>"an.ju@berkeley.edu", :birthdate => "1992-01-01", :last_name => "ju", :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now)
     @tutor = Tutor.create(:id => 1, :type_of_tutor => "20 hour TA", :grade_level => 'Junior', :first_name => 'Bart', :last_name => 'Simpson', :email => 'bart@berkeley.edu')
     @course = Course.create(:id => 1, :course_num => 1, :name => "CS61A", :semester => "Sp2019")
     @request = Request.create(:id => 1, :tutee_id => 1, :course_id => 1, :subject => "tree")

d6d0d3e7fdd19f9e82a78fabaf4e7ebd5d4f824f
cdvillegas <49659443+cdvillegas@users.noreply.github.com>
2019-12-13 18:27:10 -0800

    Reverted meeting filtering (#22)

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -23,11 +23,8 @@ class TutorsController < ApplicationController
     @testing = @test.map{|req| req.evaluation.nil?}
     @abc = @testing.last
 
-    @meeting_times = @meetings.map{|meet| meet.set_time.strftime("on %A %d at %l:%M %p")}
+    @meeting_times = @meetings.map{|meet| meet.set_time.strftime("%A %d at %l:%M %p")}
     @meeting_locations = @meetings.map{|meet| meet.set_location.titleize}
-    @meeting_evals = @meetings.map{|meet| Evaluation.find_by_id(meet.evaluation_id)}
-    @meeting_evals_status = @meeting_evals.map{|eval| eval.status}
-    @meeting_evals_took_place = @meeting_evals.map{|eval| eval.took_place}
     @meeting_tutees = @meetings.map{|meet| Tutee.find_by_id(meet.tutee_id)}
     @meeting_emails = @meeting_tutees.map{|tutee| tutee.email}
     @meeting_names = @meeting_tutees.map{|tutee| tutee.first_name + " " + tutee.last_name}

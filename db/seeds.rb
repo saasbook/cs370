52e14955228991419cdbb1d93d8a4ff43476cb55
Nicholas Figueira <nfigueira@berkeley.edu>
2021-03-30 16:30:56 -0700

    removed space from semester in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 course_list = ["CS10", "CS61A", "CS61B", "CS61C", "CS70", "CS88", "EE16A", "EE16B", "DATA8", "UPPERDIV", "OTHER"]
 
 course_list.each_with_index do |course, i|
-  Course.create!(:course_num => i + 1, :name => course, :semester => "Spring 2021")
+  Course.create!(:course_num => i + 1, :name => course, :semester => "Spring2021")
 end
 
 

5aa2006011fa0073cf2ba4f14c9dde5b3d9c0438
Austin <wangyu.austin@gmail.com>
2021-04-30 15:12:57 -0700

    never use chromedriver. buggy af. added geckodriver installation to before_install in travis.yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,8 @@ SimpleCov.start
 
 require 'cucumber/rails'
 require 'webdriver'
-Capybara.javascript_driver = :selenium_headless
+
+Capybara.javascript_driver = :selenium
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.

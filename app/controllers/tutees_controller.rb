df36cce9cf6e2390dc67305c3e3a264cbd84ad8f
Austin <wangyu.austin@gmail.com>
2021-04-30 21:30:27 -0700

    im pretty sure this is less readable than it was before but whatever makes codeclimate happy i guess

--- app/controllers/tutees_controller.rb
+++ app/controllers/tutees_controller.rb
@@ -14,18 +14,30 @@ class TuteesController < ApplicationController
     if params[:id] == "password"
       redirect_to new_tutee_password_path
     else
-      current_semester = Course.current_semester
-      @courses = [Course.find_by_semester(current_semester)]
+      @courses = show_helper_courses
       @tutee = Tutee.find_by_id(params[:id])
-      @requests = @tutee.requests.where('created_at >= ?', Date.today.beginning_of_week.strftime("%Y-%m-%d"))
+      @requests = show_helper_requests @tutee
       @evaluations = @tutee.evaluations
       @meeting = Meeting.where(request_id: @requests).last
-      if not @meeting.nil?
-        @lastEval = Evaluation.find_by_id( @meeting.evaluation_id)
-        if @lastEval.status == "Complete"
-          @meeting = nil
-          return
-        end
+      @meeting = show_helper_meeting_processing @meeting
+    end
+  end
+
+  def show_helper_courses
+    [Course.find_by_semester(Course.current_semester)]
+  end
+
+  def show_helper_requests tutee
+    tutee.requests.where('created_at >= ?', Date.today.beginning_of_week.strftime("%Y-%m-%d"))
+  end
+
+  def show_helper_meeting_processing meeting
+    if not meeting.nil?
+      @lastEval = Evaluation.find_by_id( meeting.evaluation_id)
+      if @lastEval.status == "Complete"
+        return nil
+      else
+        return meeting
       end
     end
   end

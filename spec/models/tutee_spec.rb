2ea307a96ee5c150bda93fa00a1a8e03dcc73928
Austin <wangyu.austin@gmail.com>
2021-05-05 16:08:59 -0700

    deleted rspec, realized that cucumber basically tests everything anyway

--- spec/models/tutee_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Tutee, type: :model do
-  it "creates a new tutee" do
-	@tutee = Tutee.create(:sid=>1234567, :first_name=>"an", :email=>"an.ju@berkeley.edu", :last_name => "ju", :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now)
-	expect(@tutee).not_to be_nil
-  end
-end
